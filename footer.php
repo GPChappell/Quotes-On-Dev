8d0b9e0ed56265bf08f3b0d1e7e21ba6969f31be
Guy Chappell <mettcat@gmail.com>
2018-02-28 10:54:13 -0800

    nav added. page-submit.php added

---
+++ footer.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * @package QOD_Starter_Theme
+ */
+
+?>
+
+			</div><!-- #content -->
+
+			<footer id="colophon" class="site-footer" role="contentinfo">
+				<div class="site-info">
+
+					<?php	
+						wp_nav_menu(
+							array(
+								'theme_location' => 'primary',
+								'menu_id' => 'primary_menu',
+								'menu_class' => 'footer_navigation'
+							)
+						);
+					?>
+
+					<p>Brought to you by &copy; <a href="www.redacademy.com">RED Academy</a> <?php echo date( 'Y' ); ?></p>
+				</div><!-- .site-info -->
+			</footer><!-- #colophon -->
+		</div><!-- #page -->
+
+		<?php wp_footer(); ?>
+
+	</body>
+</html>
