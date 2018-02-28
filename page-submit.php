b21cf033ab78a7a1b0763c1cad3b5e5aa718a15e
Guy Chappell <mettcat@gmail.com>
2018-02-28 11:48:46 -0800

    Added font awesome. Created rest end point and nonce

--- page-submit.php
+++ page-submit.php
@@ -18,17 +18,21 @@ get_header(); ?>
         <?php if( is_user_logged_in() && current_user_can( 'edit_posts' ) ): ?>
 
         <form name="quoteForm" id="quote-submission-form">
+
           <div>
             <label for="quote-author">Author of Quote</label>
             <input type="text" name="quote_author" id="quote_author">
           </div>
+
           <div>
             <textarea name="quote_content" id="quote-content" cols="20" rows="3"></textarea>
           </div>
+
           <div>
             <label for="quote-source">Where did you find this quote? (e.g. book name)</label>
             <input type="text" name="quote_source" id="quote-source">
           </div>
+          
           <div>
             <label for="quote-source-url">Provide the URL of the quote source, if available.</label>
             <input type="url" name="quote_source_url" id="quote-source-url">
