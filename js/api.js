230547143cb2b93f86443da516493713685a9ffa
Guy Chappell <mettcat@gmail.com>
2018-03-02 14:34:26 -0800

    Browser history updates working

--- js/api.js
+++ js/api.js
@@ -12,40 +12,67 @@
 
 (function ($) {
 
+  var lastPage = '';
+  var postData;
+
   $('#new-quote-button').on('click', function (e) {
       e.preventDefault();
+
+      lastPage = document.URL;
 
       $.ajax({
       method: 'GET',
       url: api_vars.root_url + 'wp/v2/posts?filter[orderby]=rand&filter[posts_per_page]=1',
     })
     .done(function (response) {
-      console.log( response );
-      var postData = response[0];
-      $('.home .entry-content p').replaceWith( postData.content.rendered );
-      $('.home .entry-content .entry-meta .entry-title').replaceWith( '<h2 class="entry-title">&mdash; '+postData.title.rendered+'</h2>' );
+
+      console.log(response);
+
+      postData = response[0];
+      // console.log(postData);
+      var url = api_vars.home_url + '/' + postData.slug + '/';
+
+      
+      
+
+      $('.entry-content p').replaceWith( postData.content.rendered );
+      $('.entry-content .entry-meta .entry-title').replaceWith( '<h2 class="entry-title">&mdash; '+postData.title.rendered+'</h2>' );
 
       if( postData._qod_quote_source.length !== 0 && postData._qod_quote_source_url.length !== 0 ) {
-        $('.home .entry-content .entry-meta .source').replaceWith( '<span class="source">, <a href="'+postData._qod_quote_source_url+'">'+ postData._qod_quote_source +'</a></span>' );
-        console.log('source & url');
+        $('.entry-content .entry-meta .source').replaceWith( '<span class="source">, <a href="'+postData._qod_quote_source_url+'">'+ postData._qod_quote_source +'</a></span>' );
       }
       else if( postData._qod_quote_source.length !== 0 ) {
-        $('.home .entry-content .entry-meta .source').replaceWith( '<span class="source">, '+ postData._qod_quote_source +'</span>' );
-        console.log('source & url');
+        $('.entry-content .entry-meta .source').replaceWith( '<span class="source">, '+ postData._qod_quote_source +'</span>' );
       }
       else {
-        $('.home .entry-content .entry-meta .source').replaceWith( '<span class="source"></span>' );
+        $('.entry-content .entry-meta .source').replaceWith( '<span class="source"></span>' );
       }
 
+      history.pushState(null, null, url);
 
-      console.log(postData._qod_quote_source);
-      console.log(postData._qod_quote_source.length);      
-      console.log(postData._qod_quote_source_url);
-      console.log(postData._qod_quote_source_url.length);           
-      console.log(postData);
     }).fail( function() {
       console.log('fail');
-    }
-    );
+    });
+
+    // window.onpopstate = function(event) {
+    //   if (window.location.hash.indexOf('qm-overview') === 1) {
+    //     return false;
+    //   }else {
+    //     window.location.replace(lastPage);
+    //   }
+    // };
+
+   
+
   });
+
+  $(window).on('popstate', function() {
+    console.log("popstate fired!");
+    if (window.location.hash.indexOf('qm-overview') === 1) {
+      return false;
+    }else {
+      window.location.replace(lastPage);
+    }
+  });
+
 })(jQuery);
