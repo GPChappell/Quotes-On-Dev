23a7a022e5a175479b0115764028fc1fda4b7591
Guy Chappell <mettcat@gmail.com>
2018-03-04 17:17:34 -0800

    Comment and clean up api.js

--- js/api.js
+++ js/api.js
@@ -1,34 +1,31 @@
-// (function($) {
-
-
-    //Fetch a random quote post
-
-//history API, History.pushState()
-
-// Submit a new quote with the form using ajax
-
-
-// })(jQuery);
-
+/**
+ * Retrieve a new random post on the home page.
+ * AJAX get call fires on button click to retrieve random post via WP REST API.
+ * Front page is updated without refresh via jQuery.
+ * HistoryAPI is used to update page URL with each new quote.
+ */
 (function ($) {
 
   var lastPage = '';
   var postData;
 
+  //Click event to retrieve new quote
   $('#new-quote-button').on('click', function (e) {
       e.preventDefault();
 
-      lastPage = document.URL;
+      //Store current URL to enable later backtracking
+      lastPage.push = document.URL;
 
+      //AJAX get random quote
       $.ajax({
       method: 'GET',
       url: api_vars.root_url + 'wp/v2/posts?filter[orderby]=rand&filter[posts_per_page]=1',
-      })
-      .done(function (response) {
+      success: function( response ) {
 
         postData = response[0];
         var url = api_vars.home_url + '/' + postData.slug + '/';
 
+        //Update front page with new quote
         $('.entry-content p').replaceWith( postData.content.rendered );
         $('.entry-content .entry-meta .entry-title').html( '&mdash; '+postData.title.rendered );
 
@@ -44,12 +41,18 @@
 
         history.pushState(null, null, url);
 
-      }) //.done()
-      .fail( function() {
-        console.log('fail');
+      },
+      error: function() {
+
+        //Display error message
+        $('.entry-content p').replaceWith( '</p>Sorry, new post could not be retrieve</p>' );
+        $('.entry-content .entry-meta .entry-title').html( '' );
+        $('.entry-content .entry-meta .source').html( '' );
+      }
       });
 
-      window.onpopstate = function(event) {
+      //Update URL when new quote is displayed
+      window.onpopstate = function(e) {
         if (window.location.hash.indexOf('qm-overview') === 1) {
           return false;
         }
@@ -61,6 +64,10 @@
 })(jQuery);
 
 
+/**
+ * Submit new quote to database.
+ * AJAX post sent to WP REST API endpoint to create new post
+ */
 (function ($) {
 
   $('#submit-quote-button').on('click', function (e) {
@@ -71,29 +78,63 @@
     var quoteSource = $('#quote-source').val();
     var quoteSourceURL = $('#quote-source-url').val();
 
-    data = {
+    var data = {
       status: 'pending',
       title: quoteAuthor,
       content: quoteContent,
       _qod_quote_source: quoteSource,
       _qod_quote_source_url: quoteSourceURL
-    }
+    };
 
     $.ajax({
       method: 'post',
       url: api_vars.root_url + 'wp/v2/posts/',
       data: data,
-      success: function (response) {
+      beforeSend: function (xhr) {
+          xhr.setRequestHeader('X-WP-Nonce', api_vars.nonce);
+      },
+      success: function () {
         $('#quote-submission-form').hide('slow');
         $('.entry-title').after( '<p>'+api_vars.success+'</p>' );
       },
-      error: function (response) {
+      error: function () {
         $('#quote-submission-form').hide('slow');
         $('.entry-title').after( '<p>'+api_vars.failure+'</p>' );
-      },
-      beforeSend: function (xhr) {
-          xhr.setRequestHeader('X-WP-Nonce', api_vars.nonce);
       }
     });
   });
 })(jQuery);
+
+/**
+ * Register a Custom Rest endpoint
+ * route: wp-json/redsprout/v1/votes
+ */
+// function redsprout_register_endpoints() {
+// 	register_rest_route(
+// 		'redsprout/v1',
+// 		'/votes/',
+// 		array(
+// 			'methods'  => 'GET',
+// 			'callback' => 'redsprout_add_vote',
+// 		)
+// 	);
+// }
+
+/**
+ * Basic vote testing no authentication or validation
+ * Test with postman, the id of the post and number
+ * Check your postmeta table for stored values
+ *
+ * in postman use post request with Body and form-data
+ * form-data will needs the id of the post e.g. id 42
+ */
+// function redsprout_add_vote( WP_REST_Request $request ) {
+// 	$votes = intval( get_post_meta( $request->get_param( 'id' ), 'votes', true ) );
+// 	// if false === (bool) checks if update is successful, not false
+// 	if ( false === (bool) update_post_meta( $request->get_param( 'id' ), 'votes', $votes + 1 ) ) {
+// 		return new WP_Error( 'vote_error', __( 'Unable to add vote', 'redsprout' ), $request->get_param( 'id' ) );
+// 	}
+
+// 	// if no errors, increment vote
+// 	return $votes + 1;
+// }
